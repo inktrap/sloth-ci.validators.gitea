8a1483f01f15cc2c43be293ae12579ac2c13f033
inktrap <inktrap@inktrap.org>
2021-10-23 12:52:23 +0200

    gitea headers and ips

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # sloth-ci.validators.gitea
 
 Sloth CI validator for [Gitea](https://gitea.io.com/) [push](https://docs.gitea.io/en-us/webhooks/) events.
+Gitea sends the same [payload data](https://docs.gitea.io/en-us/webhooks/) triggered by push hooks as GitHub.
 
+TODO: ipv6?
+```
+            - 2a03:4000:4c:e24:85e:10ff:fef8:a405
+```
 
 ## Installation
 
@@ -15,12 +20,14 @@ $ pip install sloth-ci.validators.gitea
 ```
 provider:
     gitea:
-        # Whitelisted Gitea server IPs.
-        # Optional parameter, since Gitea is a self-hosted service or might be
-        # used via e.g. codeberg.org
+        # Repository owner. Mandatory parameter.
+        owner: moigagoo
+        # Whitelisted Gitea server IPs. Mandatory parameter.
+        # This is only codeberg.org and localhost by default
+        # but must be extended as you see fit if you self-host Gitea
         trusted_ips:
-            - 123.45.67.89
-            - 111.22.33.44
+            - 193.26.156.135
+            - 127.0.0.1
 
         # Repository title as it appears in the URL, i.e. slug.
         # Mandatory parameter.
--- gitea.py
+++ gitea.py
@@ -1,49 +1,94 @@
-
-
-__title__ = 'sloth-ci.validators.gitlab'
-__description__ = 'GitLab validator for Sloth CI'
-__version__ = '1.0.2'
-__author__ = 'Vladimir Akritskiy'
-__author_email__ = 'lenin.lin@gmail.com'
-__license__ = 'MIT'
+# TODO: check for headers
+# X-Gitea-Delivery: f6266f16-1bf3-46a5-9ea4-602e06ead473
+# X-Gitea-Event: push
+# see: <https://docs.gitea.io/en-us/webhooks/>
 
 
 def validate(request, validation_data):
-    '''Check payload from GitLab: the origin IP must be genuine and the repo title must be valid.
+    """Check payload from Gitea: the origin IP must be genuine; the repo owner and title must be valid.
 
     :param request: `CherryPy request <http://docs.cherrypy.org/en/latest/pkg/cherrypy.html#cherrypy._cprequest.Request>`_ instance representing incoming request
-    :param validation_data: dict with the keys ``trusted_ips``, ``repo``, and ``branches``, parsed from the config
+    :param validation_data: dict with the keys ``owner`` ``trusted_ips``, ``repo``, and ``branches``, parsed from the config
 
     :returns: namedtuple(status, message, list of extracted params as dicts), e.g. ``Response(status=200, message='Payload validated. Branches: default', [{'branch': 'default'}])``
-    '''
+
+    """
 
     from collections import namedtuple
 
+    from ipaddress import ip_address, ip_network
 
-    response = namedtuple('Response', ('status', 'message', 'param_dicts'))
+    response = namedtuple("Response", ("status", "message", "param_dicts"))
 
-    if request.method != 'POST':
-        return response(405, 'Payload validation failed: Wrong method, POST expected, got %s.' % request.method, [])
+    if request.method != "POST":
+        return response(
+            405,
+            "Payload validation failed: Wrong method, POST expected, got %s."
+            % request.method,
+            [],
+        )
 
-    if request.remote.ip not in validation_data['trusted_ips']:
-        return response(403, 'Payload validation failed: Unverified remote IP: %s.' % remote_ip, [])
+    this_header = "X-Gitea-Delivery"
+    if this_header not in request.header:
+        return response(
+            403,
+            "Header validation failed: Header not present: %s" % this_header,
+            [],
+        )
+
+    this_header = "X-Gitea-Event"
+    if this_header not in request.header:
+        return response(
+            403,
+            "Header validation failed: Header not present: %s" % this_header,
+            [],
+        )
+
+    # remote_ip = ip_address(request.remote.ip)
+    if request.remote.ip not in validation_data["trusted_ips"]:
+        return response(
+            403,
+            "Payload validation failed: Unverified remote IP: %s." % request.remote.ip,
+            [],
+        )
 
     try:
         payload = request.json
 
-        repo = payload['repository']['name']
+        is_ping = "zen" in payload
 
-        if repo != validation_data['repo']:
-            return response(403, 'Payload validation failed: wrong repository: %s' % repo, [])
+        if is_ping:
+            owner = payload["repository"]["owner"]["login"]
+        else:
+            owner = payload["repository"]["owner"]["name"]
 
-        branch = {payload['ref'].split('/')[-1]}
+        if owner != validation_data["owner"]:
+            return response(
+                403, "Payload validation failed: wrong owner: %s" % owner, []
+            )
 
-        allowed_branches = set(validation_data.get('branches', branch))
+        repo = payload["repository"]["name"]
 
-        if not branch & allowed_branches:
-            return response(403, 'Payload validation failed: wrong branch: %s' % branch, [])
+        if repo != validation_data["repo"]:
+            return response(
+                403, "Payload validation failed: wrong repository: %s" % repo, []
+            )
 
-        return response(200, 'Payload validated. Branch: %s' % branch, [{'branch': branch}])
+        if is_ping:
+            return response(200, "Ping payload validated", [])
+
+        branch = payload["ref"].split("/")[-1]
+
+        allowed_branches = set(validation_data.get("branches", branch))
+
+        if branch not in allowed_branches:
+            return response(
+                403, "Payload validation failed: wrong branch: %s" % branch, []
+            )
+
+        return response(
+            200, "Payload validated. Branch: %s" % branch, [{"branch": branch}]
+        )
 
     except Exception as e:
-        return response(400, 'Payload validation failed: %s' % e, [])
+        return response(400, "Payload validation failed: %s" % e, [])
